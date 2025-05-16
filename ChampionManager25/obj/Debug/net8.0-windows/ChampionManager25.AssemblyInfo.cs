ef5c8ffebd8d4464e96156b2a1c5f7a69703790b
BuitragoDev <177125589+BuitragoDev@users.noreply.github.com>
2025-05-16 16:04:18 +0200

    Agregadas las fotos hasta los jugadores del AC Milan

--- ChampionManager25/obj/Debug/net8.0-windows/ChampionManager25.AssemblyInfo.cs
+++ ChampionManager25/obj/Debug/net8.0-windows/ChampionManager25.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChampionManager25")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fa5dd25247489f31ebbe8af8d8adf8b52fb3e0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ab0b1e0fe9c5ae010847dcafa9002c04fa95656")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChampionManager25")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChampionManager25")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
